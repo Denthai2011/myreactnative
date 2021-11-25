1f48deac88991b1f324f4f6ba509045942057aa7
Denthai2011 <ynloveys@gmail.com>
2021-11-25 17:28:51 +0700

    week2

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Welcome from './screens/Welcome';
-import Ex9 from './screens/week2/Ex9';
+
 export default function App() {
   return (
-    <Ex9 />
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
