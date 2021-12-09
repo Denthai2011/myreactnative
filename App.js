2f21a8127444efd69cd27cf4d716aed81763451f
Denthai2011 <ynloveys@gmail.com>
2021-12-09 11:47:20 +0700

    update

--- App.js
+++ App.js
@@ -3,17 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Welcome from './screens/Welcome';
 import Ex9 from './screens/week2/Ex9';
+import Travel from './screens/week3/Travel';
 export default function App() {
   return (
-    <Ex9 />
+    <Travel/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
