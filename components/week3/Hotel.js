f958b4338d843beb4e7ab917454da4a9e73cd7bf
Denthai2011 <ynloveys@gmail.com>
2021-12-09 18:34:37 +0700

    week 4

---
+++ components/week3/Hotel.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View,Image } from 'react-native';
+
+export default function Hotel() {
+    return (
+
+            <ScrollView>
+             
+           
+            </ScrollView>
+
+    );
+}
--- screens/week3/Travel.js
+++ screens/week3/Travel.js
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import Signup from '../../components/week3/Signup';
-
+import Card from '../../components/week3/Card';
+import Resort from '../weeek4/Resort';
 export default function Travel() {
     return (        
-        <View style={{ flex: 1, flexDirection: 'column'}}>
+        <View style={{ flex: 1, paddingTop: 50}}>
             {/* <Text> Week 3 </Text> */}
-            <Signup />
+            {/* <Signup /> */}
+            {/* <Card/> */}
+            <Resort/>
         </View>
     );
 }
