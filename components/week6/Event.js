0af72411d7b72209b5255a9bc2993d0ccef9f26d
Denthai2011 <ynloveys@gmail.com>
2021-12-23 17:42:43 +0700

    week6 update

--- components/week6/Event.js
+++ components/week6/Event.js
@@ -1,55 +1,72 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, Image, FlatList, ScrollView } from "react-native";
 
 export default function Event(props) {
-  const tours = [
-    {
-      id: "1",
-      title: "Truckfighters: Performing",
-      uri: "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-1.jpg",
-      month: "DEC",
-      date: "30",
-      datetime: "Thu, DEC 30, 09.00 am",
-      place: "London",
-    },
-    {
-      id: "2",
-      title: "Paris Motor Show",
-      uri: "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-2.jpg",
-      month: "DEC",
-      date: "31",
-      datetime: "Thu, DEC 30, 09.00 am",
-      place: "Paris",
-    },
-    {
-      id: "3",
-      title: "Bearded Theory Spring",
-      uri: "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-3.jpg",
-      month: "JAN",
-      date: "01",
-      datetime: "Thu, JAN 01, 09.00 am",
-      place: "Canada",
-    },
-    {
-      id: "4",
-      title: "BBC Music Introducing",
-      uri: "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-4.jpg",
-      month: "JAN",
-      date: "11",
-      datetime: "Thu, JAN 11, 09.00 am",
-      place: "USA",
-    },
-    {
-      id: "5",
-      title: "Start-Up Meeting 2022",
-      uri: "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-5.jpg",
-      month: "JAN",
-      date: "21",
-      datetime: "Thu, JAN 21, 09.00 am",
-      place: "Thailand",
-    },
-  ];
+    const [onlineTours, setOnlineTours] = useState([]);
+    const loadOnlineTours = async () => {
+      try{
+          let promise = await fetch('https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/json/events.json ');
+          let data = await promise.json();
+          console.log("Load Data : " , data);
+        //SET STATE
+          setOnlineTours(data);
+      }catch(error){
+          console.log("ERROR : " , error);
+      }
+      
+    
+  }  
+  useEffect(() => {
+        loadOnlineTours();
+    }, []);
+//     const tours = [
+//     {
+//       id: "1",
+//       title: "Truckfighters: Performing",
+//       uri: "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-1.jpg",
+//       month: "DEC",
+//       date: "30",
+//       datetime: "Thu, DEC 30, 09.00 am",
+//       place: "London",
+//     },
+//     {
+//       id: "2",
+//       title: "Paris Motor Show",
+//       uri: "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-2.jpg",
+//       month: "DEC",
+//       date: "31",
+//       datetime: "Thu, DEC 30, 09.00 am",
+//       place: "Paris",
+//     },
+//     {
+//       id: "3",
+//       title: "Bearded Theory Spring",
+//       uri: "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-3.jpg",
+//       month: "JAN",
+//       date: "01",
+//       datetime: "Thu, JAN 01, 09.00 am",
+//       place: "Canada",
+//     },
+//     {
+//       id: "4",
+//       title: "BBC Music Introducing",
+//       uri: "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-4.jpg",
+//       month: "JAN",
+//       date: "11",
+//       datetime: "Thu, JAN 11, 09.00 am",
+//       place: "USA",
+//     },
+//     {
+//       id: "5",
+//       title: "Start-Up Meeting 2022",
+//       uri: "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/all/event-5.jpg",
+//       month: "JAN",
+//       date: "21",
+//       datetime: "Thu, JAN 21, 09.00 am",
+//       place: "Thailand",
+//     },
+//   ];
 
   return (
     <View style={props.style}>
@@ -59,7 +76,7 @@ export default function Event(props) {
       </Text>
       <FlatList
         horizontal={true}
-        data={tours}
+        data={onlineTours}
         renderItem={({ item, index }) => {
           console.log(item, index, item.uri);
           return (
