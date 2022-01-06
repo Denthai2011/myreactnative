ae122566b413488b6c2313b234fa69280f6ffc74
Denthai2011 <ynloveys@gmail.com>
2022-01-06 17:38:02 +0700

    weak 7

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import RootStack from "./navigations/RootStack";
+import HomeStack from './navigations/HomeStack';
+import BottomTab from './navigations/BottomTab';
 import { StyleSheet, Text, View } from 'react-native';
 import Welcome from './screens/Welcome';
 import Ex9 from './screens/week2/Ex9';
@@ -8,9 +12,11 @@ import Health from './screens/week5/Health';
 import Home from './screens/week6/Home';
 export default function App() {
   return (
-    // <Travel/>
-    // <Health/>
-    <Home/>
+    <NavigationContainer>
+            {/* <HomeStack /> */}
+            {/* <BottomTab /> */}
+            <RootStack />
+        </NavigationContainer>
     
   );
 }
